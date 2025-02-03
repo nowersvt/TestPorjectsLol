0d9d796908fbf078c8c8a2a5741fd12e883b5020
Mykyta <hiban.com@gmail.com>
2025-02-03 15:09:07 +0100

    sm fix

--- dbURL.go
+++ dbURL.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func dbURL(filename string) (string, error) {
-    config, err := LoadConfig(filename)
-    if err != nil {
-        return "", err
-    }
+	config, err := LoadConfig(filename)
+	if err != nil {
+		return "", err
+	}
 
-    fmt.Printf("Loaded Config: %+v\n", config)
+	fmt.Printf("Loaded Config: %+v\n", config)
 
-    host := "localhost"
-    user := config.Postgres.Environment.User
-    password := config.Postgres.Environment.Password
-    dbName := config.Postgres.Environment.DbName
-    port, err := extractPort(config.Postgres.Ports)
-    if err != nil {
-        return "", err
-    }
+	host := "localhost"
+	user := config.Postgres.Services.Environment.User
+	password := config.Postgres.Services.Environment.Password
+	dbName := config.Postgres.Services.Environment.DbName
+	port, err := extractPort(config.Postgres.Services.Ports)
+	if err != nil {
+		return "", err
+	}
 
-    fmt.Println("Host:", host)
-    fmt.Println("User:", user)
-    fmt.Println("Password:", password)
-    fmt.Println("Database:", dbName)
-    fmt.Println("Port:", port)
+	fmt.Println("Host:", host)
+	fmt.Println("User:", user)
+	fmt.Println("Password:", password)
+	fmt.Println("Database:", dbName)
+	fmt.Println("Port:", port)
 
-    connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", user, password, host, port, dbName)
-    return connStr, nil
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", user, password, host, port, dbName)
+	return connStr, nil
 }
